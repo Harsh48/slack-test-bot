a69c93f88b1e760830bccb9b34626b6e39b95fab
Harsh48 <harsh.techilia@gmail.com>
2023-11-10 17:50:52 +0530

    cron job config

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const { App } = require("@slack/bolt");
-// const bigquery = require("./bigQuery");
 const { BigQuery } = require("@google-cloud/bigquery");
 const { project_id, client_email, private_key } = require("./constants");
+const cron = require("node-cron");
 
 require("dotenv").config();
 
@@ -20,10 +20,18 @@ const bigquery = new BigQuery({
 	},
 });
 
-function sendMessage() {
+app.message("hello", async ({ message, say }) => {
+	try {
+		await say("Hey there");
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+function sendMessage(channel, message) {
 	app.client.chat.postMessage({
-		channel: "internal",
-		text: "This is a test message!",
+		channel: channel,
+		text: message,
 	});
 }
 
@@ -31,39 +39,179 @@ function sendMessage() {
 	// Start your app
 	await app.start(process.env.PORT || 3000);
 
-	console.log("⚡️ Bolt app is running!");
-
-	sendMessage();
+	console.log("⚡️ App is running!");
 })();
 
-app.message("hello", async ({ message, say }) => {
+const date = new Date();
+const yesterday = new Date(date.setDate(date.getDate() - 1));
+const sevenDaysAgo = new Date(date.setDate(date.getDate() - 7));
+const formattedDate = yesterday.toISOString().split("T")[0];
+
+const sevenDaysAgoFormattedDate = sevenDaysAgo.toISOString();
+
+async function getDataNew(projectId, datasetId, tableId, startDate) {
+	const query = `
+		SELECT * from \`${projectId}.${datasetId}.${tableId}\`
+		WHERE date > '2023-10-08'
+	`;
+
+	const options = {
+		query: query,
+	};
+
+	const [job] = await bigquery.createQueryJob(options);
+	console.log(`Job ${job.id} started\n`);
+
+	const rows = await job.getQueryResults();
+	// console.log(rows);
+
+	return rows[0];
+}
+
+async function getDataKeywordReport(projectId, datasetId, tableId, startDate) {
+	const query = `
+		SELECT * from \`${projectId}.${datasetId}.${tableId}\`
+		WHERE to_date > CAST('2023-10-08' AS DATE)
+	`;
+
+	const options = {
+		query: query,
+	};
+
+	const [job] = await bigquery.createQueryJob(options);
+	console.log(`Job ${job.id} started\n`);
+
+	const rows = await job.getQueryResults();
+	// console.log(rows);
+
+	return rows[0];
+}
+
+async function sendCampaignInefficientNotification() {
 	try {
-		await say("Hello there!");
+		const campaignData = await getDataNew(
+			"flipkart-390013",
+			"relaxo",
+			"PLA_Consolidated_Daily_Report",
+			formattedDate
+		);
+
+		const inefficientCampaigns = [];
+
+		for (data of campaignData) {
+			const { ad_spend, direct_revenue, indirect_revenue } = data;
+			const roas = (direct_revenue + indirect_revenue) / ad_spend;
+
+			if (ad_spend > 10000 && roas < 1) {
+				// console.log("Campaign trigger");
+				inefficientCampaigns.push(data.campaign_id);
+			}
+		}
+
+		if (inefficientCampaigns.length > 0) {
+			const campaigns = inefficientCampaigns.join(", ");
+
+			sendMessage(
+				"campaign-inefficient",
+				`Your following Campaigns are not efficient:\n\n ${campaigns}`
+			);
+		}
 	} catch (error) {
 		console.log(error);
 	}
-});
-
-function getData() {
-	bigquery.getDatasets(function (err, datasets) {
-		if (err) {
-			console.log("error");
-			console.log({ err });
-		} else {
-			// datasets is an array of Dataset objects.
-			console.log(
-				datasets[0].getTables(function (err, tables) {
-					let result = [];
-
-					for (let key in tables) {
-						result.push(tables[key].id);
-					}
-					console.log(result);
-					// res.send(result);
-				})
-			);
-		}
-	});
 }
 
-getData();
+async function sendHighCpcNotification() {
+	try {
+		const campaignData = await getDataNew(
+			"flipkart-390013",
+			"relaxo",
+			"PLA_Consolidated_Daily_Report",
+			formattedDate
+		);
+
+		const highCpcCampaigns = [];
+
+		for (data of campaignData) {
+			const { ad_spend, clicks } = data;
+			const cpc = ad_spend / clicks;
+
+			if (cpc > 100) {
+				// high cpc trigger
+				highCpcCampaigns.push(data.adgroup_name);
+			}
+		}
+
+		if (highCpcCampaigns.length > 0) {
+			const campaigns = highCpcCampaigns.join(", ");
+
+			sendMessage(
+				"high-cpc",
+				`Your Cost per Click is getting expensive in the following ad groups:\n\n
+					${campaigns}`
+			);
+		} else {
+			sendMessage("high-cpc", "Your campaigns are running as expected!");
+		}
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+async function sendLowConversionRateNotification() {
+	try {
+		const campaignData = await getDataKeywordReport(
+			"flipkart-390013",
+			"relaxo",
+			"PCA_Keyword_Report",
+			sevenDaysAgoFormattedDate
+		);
+
+		const lowConversionRateCampaigns = [];
+		// console.log(campaignData);
+
+		// ((direct units+indirect units)/clicks)*100
+
+		for (data of campaignData) {
+			const { direct_converted_units, indirect_converted_units, clicks } = data;
+			const cr =
+				((direct_converted_units + indirect_converted_units) / clicks) * 100;
+
+			if (cr < 1) {
+				// low conversion rate trigger
+				lowConversionRateCampaigns.push(data.campaign_name);
+			}
+		}
+
+		if (lowConversionRateCampaigns.length > 0) {
+			const campaigns = lowConversionRateCampaigns.join(", ");
+
+			sendMessage(
+				"#low-conversion-rate",
+				`Your Conv Rate has dropped in the following targeting type:\n\n ${campaigns}`
+			);
+		} else {
+			sendMessage("#low-conversion-rate", "Your campaigns have good CR");
+		}
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+async function sendNotifications() {
+	await sendCampaignInefficientNotification();
+	await sendHighCpcNotification();
+	await sendLowConversionRateNotification();
+}
+
+cron.schedule(
+	"* * * * *",
+	async () => {
+		console.log("cron job running");
+		await sendNotifications();
+	},
+	{
+		scheduled: true,
+		timezone: "Asia/Kolkata",
+	}
+);
